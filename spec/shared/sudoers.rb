4bf6dee06f72a4911f47ed6824a0d5214e09c5e2
a-know <a.know.3373@gmail.com>
2015-12-26 18:01:13 +0900

    Add sudoers spec

---
+++ spec/shared/sudoers.rb
@@ -0,0 +1,9 @@
+shared_examples 'sudo' do
+  describe file '/etc/sudoers' do
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 440 }
+    its(:content) { should match /^%wheel ALL=\(ALL\) NOPASSWD: ALL/ }
+    its(:content) { should match /^#Defaults requiretty/ }
+  end
+end
