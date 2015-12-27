1332e8d7d8c5dacaf96ae89cf1399009b37a634f
a-know <a.know.3373@gmail.com>
2015-12-27 16:23:35 +0900

    Add openssh spec

--- spec/roles/web_spec.rb
+++ spec/roles/web_spec.rb
@@ -5,6 +5,7 @@ describe 'web' do
   it_behaves_like 'firewalld::disable'
   it_behaves_like 'git'
   it_behaves_like 'hostname'
+  it_behaves_like 'openssh'
   it_behaves_like 'ruby'
   it_behaves_like 'sudoers'
   it_behaves_like 'td-agent'
