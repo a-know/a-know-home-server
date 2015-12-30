e626e00b21ea7ac65b4944c240c48f7c2906d259
a-know <a.know.3373@gmail.com>
2015-12-30 09:53:15 +0900

    Add spec for ngin log rotation

--- spec/shared/nginx.rb
+++ spec/shared/nginx.rb
@@ -17,4 +17,12 @@ shared_examples 'nginx' do
     it { should be_grouped_into 'root' }
     it { should be_mode 644 }
   end
+
+  describe file '/etc/logrotate.d/nginx' do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 644 }
+    its(:content) { should include 'rotate 10' }
+  end
 end
