a10b06d8aa341325807be5e52b35c49e234c3824
a-know <a.know.3373@gmail.com>
2016-01-11 10:53:54 +0900

    Add spec for mackerel

---
+++ spec/shared/mackerel.rb
@@ -0,0 +1,17 @@
+shared_examples 'mackerel' do
+  describe package 'mackerel-agent' do
+    it { should be_installed }
+  end
+
+  describe service 'mackerel-agent' do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe file '/etc/mackerel-agent/conf.d/mackerel-agent.conf' do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 644 }
+  end
+end
