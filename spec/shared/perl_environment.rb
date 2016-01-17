4b17706de6cea33997ce2be6497ba1c152024524
a-know <a.know.3373@gmail.com>
2016-01-17 14:45:07 +0900

    Spec for perl-build

--- spec/shared/perl_environment.rb
+++ spec/shared/perl_environment.rb
@@ -7,4 +7,20 @@ shared_examples 'perl_environment' do
       it { should be_mode 755 }
     end
   end
+
+  describe 'perl-build' do
+    describe file '/usr/local/perlenv/.plenv/plugins' do
+      it { should be_directory }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_mode 755 }
+    end
+
+    describe file '/usr/local/perlenv/.plenv/plugins/perl-build/.git' do
+      it { should be_directory }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_mode 755 }
+    end
+  end
 end
