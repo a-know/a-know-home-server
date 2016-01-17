aa7dfbddc390716a0609df97a995970e68694f61
a-know <a.know.3373@gmail.com>
2016-01-17 14:38:47 +0900

    Add export plenv path

---
+++ site-cookbooks/perl_environment/attributes/default.rb
@@ -0,0 +1,1 @@
+default['perlenv']['user']  = nil
--- site-cookbooks/perl_environment/recipes/default.rb
+++ site-cookbooks/perl_environment/recipes/default.rb
@@ -1,9 +1,19 @@
 include_recipe 'git'
 
 directory '/usr/local/perlenv'
+user = node[:perlenv][:user]
 
 git '/usr/local/perlenv/.plenv' do
   repository 'git://github.com/tokuhirom/plenv.git'
   reference 'master'
   action :checkout
+end
+
+bash 'export plenv path' do
+  code <<-EOS
+  echo "export PATH=/usr/local/perlenv/.plenv/bin:$PATH" >> /home/#{user}/.bash_profile
+  echo 'eval "$(plenv init -)"' >> /home/#{user}/.bash_profile
+  source /home/#{user}/.bash_profile
+  EOS
+  not_if "grep '/usr/local/perlenv/.plenv/bin' /home/#{user}/.bash_profile"
 end
