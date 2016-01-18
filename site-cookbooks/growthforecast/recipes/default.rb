61e07769bdddba2b7111704371006a864f9824a8
a-know <a.know.3373@gmail.com>
2016-01-18 09:25:54 +0900

    Create cpanfile

--- site-cookbooks/growthforecast/recipes/default.rb
+++ site-cookbooks/growthforecast/recipes/default.rb
@@ -4,3 +4,9 @@ directory '/home/a-know/growthforecast' do
   mode  0755
   action :create
 end
+
+cookbook_file '/home/a-know/growthforecast/cpanfile' do
+  owner 'a-know'
+  group 'a-know'
+  mode 0644
+end
--- spec/shared/growthforecast.rb
+++ spec/shared/growthforecast.rb
@@ -5,4 +5,11 @@ shared_examples 'growthforecast' do
     it { should be_grouped_into 'a-know' }
     it { should be_mode 755 }
   end
+
+  describe file '/home/a-know/growthforecast/cpanfile' do
+    it { should be_file }
+    it { should be_owned_by 'a-know' }
+    it { should be_grouped_into 'a-know' }
+    it { should be_mode 644 }
+  end
 end
