0178cc7f0cc5ee37f8439896a8ffb81882538c9f
a-know <a.know.3373@gmail.com>
2016-01-20 23:19:53 +0900

    Create dummy cert files

--- site-cookbooks/letsencrypt/recipes/default.rb
+++ site-cookbooks/letsencrypt/recipes/default.rb
@@ -4,3 +4,52 @@ git '/usr/local/letsencrypt' do
   repository 'https://github.com/letsencrypt/letsencrypt'
   action :checkout
 end
+
+directory '/etc/letsencrypt' do
+  owner 'root'
+  group 'root'
+  mode  0755
+  action :create
+end
+
+directory '/etc/letsencrypt/archive' do
+  owner 'root'
+  group 'root'
+  mode  0700
+  action :create
+end
+
+directory '/etc/letsencrypt/archive/home.a-know.me' do
+  owner 'root'
+  group 'root'
+  mode  0755
+  action :create
+end
+
+cookbook_file '/etc/letsencrypt/archive/home.a-know.me/cert1.pem' do
+  user     'root'
+  group    'root'
+  mode     0644
+  source   'cert1.pem'
+end
+
+cookbook_file '/etc/letsencrypt/archive/home.a-know.me/chain1.pem' do
+  user     'root'
+  group    'root'
+  mode     0644
+  source   'chain1.pem'
+end
+
+cookbook_file '/etc/letsencrypt/archive/home.a-know.me/fullchain1.pem' do
+  user     'root'
+  group    'root'
+  mode     0644
+  source   'fullchain1.pem'
+end
+
+cookbook_file '/etc/letsencrypt/archive/home.a-know.me/privkey1.pem' do
+  user     'root'
+  group    'root'
+  mode     0644
+  source   'privkey1.pem'
+end
--- spec/shared/letsencrypt.rb
+++ spec/shared/letsencrypt.rb
@@ -5,4 +5,32 @@ shared_examples 'letsencrypt' do
     it { should be_grouped_into 'root' }
     it { should be_mode 755 }
   end
+
+  describe file '/etc/letsencrypt/archive/home.a-know.me/cert1.pem' do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 644 }
+  end
+
+  describe file '/etc/letsencrypt/archive/home.a-know.me/chain1.pem' do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 644 }
+  end
+
+  describe file '/etc/letsencrypt/archive/home.a-know.me/fullchain1.pem' do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 644 }
+  end
+
+  describe file '/etc/letsencrypt/archive/home.a-know.me/privkey1.pem' do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 644 }
+  end
 end
