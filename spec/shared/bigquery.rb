2bd0e2922cb40d8015b54a0113d8b68c4edf82a3
a-know <a.know.3373@gmail.com>
2016-02-04 09:22:58 +0900

    Create bigquery table schema json for rails production log

--- spec/shared/bigquery.rb
+++ spec/shared/bigquery.rb
@@ -14,4 +14,10 @@ shared_examples 'bigquery::settings' do
     it { should be_grouped_into 'root' }
     it { should be_mode 644 }
   end
+  describe file '/etc/td-agent/settings/rails_production_log_schema.json' do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 644 }
+  end
 end
