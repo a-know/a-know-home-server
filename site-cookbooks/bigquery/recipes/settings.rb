326e0645cecc6032e75a7f6d8b355f0b196f1da9
a-know <a.know.3373@gmail.com>
2016-02-07 06:40:34 +0900

    Add table json schema file for active visitor

--- site-cookbooks/bigquery/recipes/settings.rb
+++ site-cookbooks/bigquery/recipes/settings.rb
@@ -48,3 +48,10 @@ cookbook_file '/etc/td-agent/settings/hatena_star_count_schema.json' do
   mode     0644
   source   'hatena_star_count_schema.json'
 end
+
+cookbook_file '/etc/td-agent/settings/active_visitors_count_schema.json' do
+  user     'root'
+  group    'root'
+  mode     0644
+  source   'active_visitors_count_schema.json'
+end
