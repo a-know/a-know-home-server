da507f43ca5dc727b33bcadab47155059d69c2d8
a-know <a.know.3373@gmail.com>
2016-02-16 09:10:31 +0900

    Add configuration for send nginx access log to bigquery and mackerel

--- spec/shared/td-agent.rb
+++ spec/shared/td-agent.rb
@@ -15,6 +15,13 @@ shared_examples 'td-agent' do
     its(:content) { should include '@include /etc/td-agent/conf.d/*.conf' }
   end
 
+  describe file '/etc/td-agent/conf.d/grass_graph_nginx_access_log.conf' do
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 644 }
+    its(:content) { should include 'service grass-graph' }
+  end
+
   describe file '/etc/td-agent/conf.d/nginx_access_log.conf' do
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
