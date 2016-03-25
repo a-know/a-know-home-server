42aaf5d6335685435795a51b72ec0bfe5129c555
a-know <a.know.3373@gmail.com>
2016-03-25 09:31:02 +0900

    Add crontab entry for refresh crt for a-know.me

--- site-cookbooks/crontab/recipes/default.rb
+++ site-cookbooks/crontab/recipes/default.rb
@@ -1,5 +1,13 @@
 include_recipe 'crond'
 
+cron "Refresh Let's Encrypt cert-file (for a-know.me) and restart nginx" do
+  user 'root'
+  command '/usr/local/letsencrypt/letsencrypt-auto certonly --webroot -w /var/www/a-know-home/shared/public -d a-know.me --renew-by-default && sudo nginx -s reload'
+  day '25'
+  hour '4'
+  minute '00'
+end
+
 cron "Refresh Let's Encrypt cert-file (for home.a-know.me) and restart nginx" do
   user 'root'
   command '/usr/local/letsencrypt/letsencrypt-auto certonly --webroot -w /var/www/a-know-home/shared/public -d home.a-know.me --renew-by-default && sudo nginx -s reload'
