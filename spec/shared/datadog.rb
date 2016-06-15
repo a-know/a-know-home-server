e1004202c10df9a99ddfde6a7fe094a5d2ec6576
a-know <a.know.3373@gmail.com>
2016-06-15 09:33:17 +0900

    Spec for datadog working

--- spec/shared/datadog.rb
+++ spec/shared/datadog.rb
@@ -2,4 +2,18 @@ shared_examples 'datadog' do
   describe package 'datadog-agent' do
     it { should be_installed }
   end
+
+  describe service 'datadog-agent' do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe file '/etc/dd-agent/datadog.conf' do
+    it { should be_file }
+    it { should be_owned_by 'dd-agent' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode 640 }
+    its(:content) { should match /^api_key:\s.{5,}$/ }
+    its(:content) { should match /^hostname: home.a-know.me/ }
+  end
 end
