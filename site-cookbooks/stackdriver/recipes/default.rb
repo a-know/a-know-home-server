e7320c4c6891d476589ddd150e3b3c50557601bc
a-know <a.know.3373@gmail.com>
2016-06-23 16:34:35 +0900

    設定ファイルに API key をセットし、stackdriver-agent を起動

--- site-cookbooks/stackdriver/recipes/default.rb
+++ site-cookbooks/stackdriver/recipes/default.rb
@@ -1,3 +1,15 @@
 include_recipe 'yumrepo::stackdriver'
 
 package 'stackdriver-agent'
+
+stackdriver_credentials = Chef::EncryptedDataBagItem.load('credentials', 'stackdriver')
+
+template '/etc/sysconfig/stackdriver' do
+  variables stackdriver_apikey: stackdriver_credentials['api_key']
+  notifies :restart, 'service[stackdriver-agent]'
+end
+
+service 'stackdriver-agent' do
+  action [:enable, :start]
+  supports restart: true
+end
--- spec/shared/stackdriver.rb
+++ spec/shared/stackdriver.rb
@@ -5,6 +5,10 @@ shared_examples 'stackdriver' do
 
   describe service 'stackdriver-agent' do
     it { should be_enabled }
-    it { should_not be_running }
+    it { should be_running }
+  end
+
+  describe file '/etc/sysconfig/stackdriver' do
+    its(:content) { should match /^STACKDRIVER_API_KEY=".+"$/ }
   end
 end
