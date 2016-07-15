5450a13ab3aecde185b8b959987d5adf3b9e4ad4
a-know <a.know.3373@gmail.com>
2016-07-15 18:27:40 +0900

    Locate a plugin code

--- site-cookbooks/mackerel/recipes/plugin.rb
+++ site-cookbooks/mackerel/recipes/plugin.rb
@@ -8,3 +8,10 @@ cookbook_file '/etc/mackerel-agent/conf.d/check-plugins.conf' do
   mode '0644'
   notifies :restart, 'service[mackerel-agent]'
 end
+
+cookbook_file '/usr/local/bin/dice.rb' do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  notifies :restart, 'service[mackerel-agent]'
+end
--- spec/shared/mackerel.rb
+++ spec/shared/mackerel.rb
@@ -30,5 +30,12 @@ shared_examples 'mackerel' do
       it { should be_grouped_into 'root' }
       it { should be_mode 644 }
     end
+
+    describe file '/usr/local/bin/dice.rb' do
+      it { should be_file }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_mode 644 }
+    end
   end
 end
