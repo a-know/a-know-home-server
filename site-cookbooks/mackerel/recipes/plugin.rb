33f14e7987b7dce5a07670d59c9a76a05fd2e2c4
a-know <a.know.3373@gmail.com>
2016-07-15 18:32:21 +0900

    Refactor recipe

--- site-cookbooks/mackerel/recipes/plugin.rb
+++ site-cookbooks/mackerel/recipes/plugin.rb
@@ -2,23 +2,15 @@ include_recipe 'mackerel'
 
 package 'mackerel-check-plugins'
 
-cookbook_file '/etc/mackerel-agent/conf.d/check-plugins.conf' do
-  owner 'root'
-  group 'root'
-  mode '0644'
-  notifies :restart, 'service[mackerel-agent]'
-end
-
-cookbook_file '/usr/local/bin/dice.rb' do
-  owner 'root'
-  group 'root'
-  mode '0644'
-  notifies :restart, 'service[mackerel-agent]'
-end
-
-cookbook_file '/etc/mackerel-agent/conf.d/host-custom-metric-plugin.conf' do
-  owner 'root'
-  group 'root'
-  mode '0644'
-  notifies :restart, 'service[mackerel-agent]'
+%w(
+  /etc/mackerel-agent/conf.d/check-plugins.conf
+  /usr/local/bin/dice.rb
+  /etc/mackerel-agent/conf.d/host-custom-metric-plugin.conf
+).each do |file|
+  cookbook_file file do
+    owner 'root'
+    group 'root'
+    mode '0644'
+    notifies :restart, 'service[mackerel-agent]'
+  end
 end
