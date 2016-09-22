c224db48529382e785af6b0b595353e60ebea770
a-know <a.know.3373@gmail.com>
2016-09-22 17:57:20 +0900

    本家のリネームに伴う対応・crontab

--- spec/shared/crontab.rb
+++ spec/shared/crontab.rb
@@ -1,7 +1,7 @@
 shared_examples 'crontab' do
   describe cron do
-    it { should have_entry('00 4 25 * * /usr/local/letsencrypt/letsencrypt-auto certonly --webroot -w /var/www/a-know-home/shared/public -d a-know.me --renew-by-default && sudo nginx -s reload').with_user('root') }
-    it { should have_entry('00 4 15 * * /usr/local/letsencrypt/letsencrypt-auto certonly --webroot -w /var/www/a-know-home/shared/public -d home.a-know.me --renew-by-default && sudo nginx -s reload').with_user('root') }
-    it { should have_entry('00 4 10 * * /usr/local/letsencrypt/letsencrypt-auto certonly --webroot -w /var/www/a-know-home/shared/public -d grass-graph.shitemil.works --renew-by-default && sudo nginx -s reload').with_user('root') }
+    it { should have_entry('00 4 25 * * /usr/local/bin/certbot/certbot-auto certonly --webroot -w /var/www/a-know-home/shared/public -d a-know.me --renew-by-default && sudo nginx -s reload').with_user('root') }
+    it { should have_entry('00 4 15 * * /usr/local/bin/certbot/certbot-auto certonly --webroot -w /var/www/a-know-home/shared/public -d home.a-know.me --renew-by-default && sudo nginx -s reload').with_user('root') }
+    it { should have_entry('00 4 10 * * /usr/local/bin/certbot/certbot-auto certonly --webroot -w /var/www/a-know-home/shared/public -d grass-graph.shitemil.works --renew-by-default && sudo nginx -s reload').with_user('root') }
   end
 end
