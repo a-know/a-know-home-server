831128bd32f133ea2390f1af87c60e79cdf169cd
a-know <a.know.3373@gmail.com>
2016-11-19 19:10:23 +0900

    Install sysstat for iostat

--- spec/roles/web_prod_spec.rb
+++ spec/roles/web_prod_spec.rb
@@ -9,6 +9,7 @@ describe 'web' do
   it_behaves_like 'git'
   it_behaves_like 'hostname'
   it_behaves_like 'imagemagick'
+  it_behaves_like 'sysstat'
   it_behaves_like 'letsencrypt'
   it_behaves_like 'logrotate'
   it_behaves_like 'mackerel'
