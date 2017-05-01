87e2d562524915ba162c35ce7e79db73237195f4
a-know <a.know.3373@gmail.com>
2017-05-02 08:14:46 +0900

    make mounted directories

--- spec/shared/docker.rb
+++ spec/shared/docker.rb
@@ -36,4 +36,31 @@ shared_examples 'docker' do
   describe command('docker images') do
     its(:stdout) { should match /384252233704\.dkr\.ecr\.ap-northeast-1\.amazonaws\.com\/a-know-server-container-app/ }
   end
+
+  [
+    '/var/www/',
+    '/var/www/a-know-home',
+    '/var/www/a-know-home/shared',
+    '/var/www/a-know-home/shared/log',
+    '/var/www/a-know-home/shared/public',
+    '/var/www/a-know-home/shared/tmp',
+  ].each do |dir|
+    describe file dir do
+      it { should be_directory }
+      it { should be_owned_by 'a-know' }
+      it { should be_grouped_into 'a-know' }
+      it { should be_mode 755 }
+    end
+  end
+
+  [
+    '/var/www/a-know-home/shared/current',
+  ].each do |dir|
+    describe file dir do
+      it { should be_directory }
+      it { should be_owned_by 'a-know' }
+      it { should be_grouped_into 'a-know' }
+      it { should be_mode 777 }
+    end
+  end
 end
