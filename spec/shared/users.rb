859240ea7472f3137971854cd39c47dc3dcb38fd
a-know <a.know.3373@gmail.com>
2017-05-01 10:56:09 +0900

    Create ~/.aws dir

--- spec/shared/users.rb
+++ spec/shared/users.rb
@@ -25,6 +25,13 @@ shared_examples 'users' do
       it { should be_mode 755 } # GCP により作成されたばあいは 700
     end
 
+    describe file '/home/a-know/.aws' do
+      it { should be_directory }
+      it { should be_owned_by 'a-know' }
+      it { should be_grouped_into 'a-know' }
+      it { should be_mode 755 }
+    end
+
     describe file '/home/a-know/.ssh/authorized_keys' do
       it { should be_file }
       it { should be_owned_by 'a-know' }
