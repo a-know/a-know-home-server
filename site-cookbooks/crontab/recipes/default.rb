612466d30c3f9bd4e06c4819a2719598ff864f6d
a-know <a.know.3373@gmail.com>
2017-05-13 09:17:42 +0900

    Post image number service metric as each instance metric

--- site-cookbooks/crontab/recipes/default.rb
+++ site-cookbooks/crontab/recipes/default.rb
@@ -1,10 +1,10 @@
 include_recipe 'crond'
-data_bag = Chef::EncryptedDataBagItem.load('webhook_urls', 'knock')
 mackerel_credentials = Chef::EncryptedDataBagItem.load('credentials', 'mackerel')
+color = node[:instance_kind][:color]
 
 cron "Post service metric for grass--graph generated image count" do
   user 'root'
-  echo_cmd = 'echo -e "grass-graph.images.count\t$(ls -1 /var/www/a-know-home/shared/tmp/gg_others_svg/${d} | wc -l)\t$(date -u +\\%s)"'
+  echo_cmd = 'echo -e "grass-graph.' + color + '.images.count\t$(ls -1 /var/www/a-know-home/shared/tmp/gg_others_svg/${d} | wc -l)\t$(date -u +\\%s)"'
   command "export MACKEREL_APIKEY=#{mackerel_credentials['api_key']} && d=`date '+\\%Y-\\%m-\\%d'` && #{echo_cmd} | /usr/local/bin/mkr throw --service grass-graph"
   minute '*/15'
 end
