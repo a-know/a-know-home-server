4c3dbed012f0ee5b785fe58802acee8bf494d82a
a-know <a.know.3373@gmail.com>
2017-08-15 11:23:45 +0900

    Fix not_if

--- site-cookbooks/git/recipes/default.rb
+++ site-cookbooks/git/recipes/default.rb
@@ -19,5 +19,5 @@ bash 'Install git-2.14.1.tar.gz' do
   make prefix=/usr/local all
   make prefix=/usr/local install
   EOC
-  not_if { `git version | grep -e 2.14.1` }
+  not_if { File.exists?("/usr/local/src/git-2.14.1") }
 end
