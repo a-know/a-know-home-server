73bbf352479bb0c8fdfeca42314bc764617d2adf
a-know <a.know.3373@gmail.com>
2017-08-15 10:01:10 +0900

    Use curl-devel to build git

---
+++ site-cookbooks/curl-devel/metadata.rb
@@ -0,0 +1,6 @@
+name             'curl-devel'
+maintainer       'a-know'
+license          'All rights reserved'
+description      'Installs/Configures curl-devel'
+long_description 'ditto' # IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
