9945d106a5721a0a80d348871ed0e3aa6ecba017
a-know <a.know.3373@gmail.com>
2017-08-15 09:33:30 +0900

    Use gettext-devel to build git

---
+++ site-cookbooks/gettext-devel/metadata.rb
@@ -0,0 +1,6 @@
+name             'gettext-devel'
+maintainer       'a-know'
+license          'All rights reserved'
+description      'Installs/Configures gettext-devel'
+long_description 'ditto' # IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
