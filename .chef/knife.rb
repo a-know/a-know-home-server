1b288678437509807ed9cf8d0cc614cd7c72d8ec
a-know <a.know.3373@gmail.com>
2017-10-21 11:11:53 +0900

    $ bundle exec knife data bag create --secret-file data_bag_key credentials newrelic-infrastructure
    see https://github.com/thbishop/knife-solo_data_bag/issues/35

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -4,6 +4,7 @@ role_path        "roles"
 environment_path "environments"
 data_bag_path    "./data_bags"
 encrypted_data_bag_secret "data_bag_key"
+local_mode true
 
 knife[:berkshelf_path] = "cookbooks"
 Chef::Config[:ssl_verify_mode] = :verify_peer if defined? ::Chef
